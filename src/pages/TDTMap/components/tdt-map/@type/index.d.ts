a29411b58d6aee42a07b5cf661a87be31c540ef3
sikara <987555458@qq.com>
2024-04-22 15:49:34 +0800

    pref 在全局作用域中声明命名空间 T可以在任何地方使用
    经测试貌似自定义的类型有ts代码提示了

--- src/pages/TDTMap/components/tdt-map/@type/index.d.ts
+++ src/pages/TDTMap/components/tdt-map/@type/index.d.ts
@@ -1,49 +1,50 @@
-// declare module "TDT" {
-//   import { LocalSearch } from "./LocalSearch";
-//   import { Geolocation, LngLat, Marker, Polyline, TDTMap } from "./lib";
-
-//   export const Map: TDTMap;
-//   export const LocalSearch: LocalSearch;
-//   export const Geolocation: Geolocation;
-//   export const LngLat: LngLat;
-//   export const Marker: Marker;
-//   export const Polyline: Polyline;
-// }
+import { LocalSearch } from "./LocalSearch";
+import {
+  Control,
+  Geocoder,
+  InfoWindow,
+  LngLat,
+  Marker,
+  Polyline,
+  TDTGeolocation,
+  TDTMap,
+} from "./TDT";
 
 // api url: http://lbs.tianditu.gov.cn/api/js4.0/class.html
-declare namespace T {
-  // import {
-  //   TDTGeolocation,
-  //   LngLat,
-  //   Marker,
-  //   Polyline,
-  //   TDTMap,
-  //   InfoWindow,
-  //   Geocoder,
-  // } from "./TDT";
-  // import { LocalSearch } from "./LocalSearch";
+declare global {
+  declare namespace T {
+    export const Map: new (
+      container: string | HTMLElement,
+      opt?: MapOptions
+    ) => TDTMap;
 
-  const Map: typeof import("./TDT").TDTMap;
+    export const Geolocation: new () => TDTGeolocation;
 
-  const Geolocation: typeof import("./TDT").TDTGeolocation;
+    export const Geocoder: new () => Geocoder;
 
-  const Geocoder: typeof import("./TDT").Geocoder;
+    export const LngLat: new (lng: number, lat: number) => LngLat;
 
-  const LngLat: typeof import("./TDT").LngLat;
+    export const Marker: new (lnglat: LngLat) => Marker;
 
-  const Marker: typeof import("./TDT").Marker;
+    export const Polyline: new (
+      points: Array<LngLat>,
+      opt?: PolylineOptions
+    ) => Polyline;
 
-  const Polyline: typeof import("./TDT").Polyline;
+    export const LocalSearch: new (
+      map: TDTMap,
+      opt?: LocalSearchOptions
+    ) => LocalSearch;
 
-  const LocalSearch: new (
-    map: TDTMap,
-    opt?: LocalSearchOptions
-  ) => import("./LocalSearch").LocalSearch;
+    export const InfoWindow: new (
+      container: string | HTMLElement,
+      opt?: InfoWindowOptions
+    ) => InfoWindow;
 
-  const InfoWindow: new (
-    container: string | HTMLElement,
-    opt?: InfoWindowOptions
-  ) => import("./TDT").InfoWindow;
-
-  const Control: typeof import("./TDT").Control;
+    export const Control: new ({
+      position,
+    }: {
+      position: ControlPosition;
+    }) => Control;
+  }
 }
