20960bac94679630e48ca03ed2fa634cc0d12cde
sikara <987555458@qq.com>
2024-05-24 11:41:29 +0800

    fix 下拉加载逻辑
    touch结束时，scrolltop为0才能加载，反之默认取消加载；
    想支持手动缩回loading组件而取消加载，只能手动调整scrolltop处理

--- src/pages/scrollList/components/drop-down-load.tsx
+++ src/pages/scrollList/components/drop-down-load.tsx
@@ -15,7 +15,7 @@ function DropDownLoad() {
   const div = useRef<HTMLDivElement>(null);
   const ele = useRef<HTMLDivElement>(null);
   const downwarp = useRef<HTMLDivElement>(null);
-  const [a, seta] = useState(0);
+  const [loadingH, setLoadingH] = useState(0);
   const [loading, setLoading] = useState(false);
   const [moveFlag, setMoveFlag] = useState(false);
   const [list, setList] = useState(
@@ -31,43 +31,28 @@ function DropDownLoad() {
     }
     ele.current.addEventListener('touchstart', function (e) {
       e.stopPropagation();
-      if (div.current && div.current.scrollTop === 0) {
-        const { clientY, pageY, screenY } = e.changedTouches[0];
-        tmp = [clientY, pageY, screenY];
-        setMoveFlag(true);
-      }
+      const { clientY, pageY, screenY } = e.changedTouches[0];
+      tmp = [clientY, pageY, screenY];
+      setMoveFlag(true);
     });
-    // ele.current.addEventListener('touchmove', function (e) {
-    //   if (!moveFlag) return;
-    //   e.stopPropagation();
-    //   const { clientY, pageY, screenY } = e.changedTouches[0];
-    //   const [clientY1, pageY1, screenY1] = tmp;
-    //   const h = Math.round(clientY - clientY1);
-    //   if (h > 0) {
-    //     seta(() => Math.min(h, 60));
-    //     setLoading(true);
-    //   } else {
-    //     seta(() => Math.max(a + h, 0));
-    //     if (Math.max(a + h, 0) === 0) {
-    //       setLoading(false);
-    //     }
-    //   }
-    // });
 
     ele.current.addEventListener('touchend', async function (e) {
       e.stopPropagation();
-      const item = await addItem(list);
-      setList([...item]);
+      // 能看到loading，只有在屏幕顶部才进行加载数据
+      if (div.current && div.current.scrollTop === 0 && loadingH > 0 && loading) {
+        const item = await addItem(list);
+        setList([...item]);
+      }
       setLoading(false);
-      seta(0);
+      setLoadingH(0);
       setMoveFlag(false);
     });
-  }, [a, list]);
+  }, [loadingH, list]);
 
   return (
     // 子元素的滚动溢出影响其父级元素的滚动---overscroll-contain解决
     <div className={`m-4 w-[200px] h-96 bg-green-400 overflow-y-auto overscroll-contain`} ref={div}>
-      <div id="refreshText" className="bg-purple-400" style={{ height: a }} ref={downwarp}>
+      <div id="refreshText" className="bg-purple-400" style={{ height: loadingH }} ref={downwarp}>
         {loading && <div>loading </div>}
       </div>
       <div
@@ -80,18 +65,21 @@ function DropDownLoad() {
           const { clientY, pageY, screenY } = e.changedTouches[0];
           const [clientY1, pageY1, screenY1] = tmp;
           const h = Math.round(clientY - clientY1);
+          // const q = Math.round(pageY - pageY1);
+          // const h2 = Math.round(screenY - screenY1);
           console.log('div.current?.scrollTop', div.current?.scrollTop);
+          // console.log('h', h);
+          // 避免增加的组件高度导致多余的滚动而无法缩回loading组件
+          div.current!.scrollTop = div.current!.scrollTop - loadingH;
           if (h > 0) {
-            seta(() => Math.min(h, 60));
+            setLoadingH(() => Math.min(h, 60));
             setLoading(true);
+          } else {
+            setLoadingH(() => Math.max(loadingH + h, 0));
+            if (Math.max(loadingH + h, 0) === 0) {
+              setLoading(false);
+            }
           }
-          // else {
-          //   div.current!.scrollTop = Math.max(a + h, 0);
-          //   seta(() => Math.max(a + h, 0));
-          //   if (Math.max(a + h, 0) === 0) {
-          //     setLoading(false);
-          //   }
-          // }
         }}
       >
         {list.map((o) => {
