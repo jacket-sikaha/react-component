443421097a47d0174f77788f4703a8107780b034
sikara <987555458@qq.com>
2025-12-08 09:30:52 +0800

    fix 滚动条样式问题

--- src/components/circle-slider/index.tsx
+++ src/components/circle-slider/index.tsx
@@ -42,6 +42,28 @@ function CircleSlider({ value = 50, min = 30, max = 60, onChange = (v) => {} }) 
     });
     setPercent(() => Math.round(angleRadians2Percent(radian) * 100));
   }, [value]);
+
+  const handleMove = (dxTemp: number, dyTemp: number) => {
+    if (!flag) return;
+    // 计算拖动的弧度值
+    const rawAngleRadians = Math.atan2(dyTemp - center, dxTemp - center);
+    console.log('rawAngleRadians:', rawAngleRadians);
+    const percent = angleRadians2Percent(rawAngleRadians);
+    if (percent >= 0.9) {
+      return;
+    }
+    setPercent(() => Math.round(percent * 100));
+    // 根据角度和半径计算新的位置
+    setPosition({
+      dx: Math.round(radius + radius * Math.cos(rawAngleRadians)),
+      dy: Math.round(radius + radius * Math.sin(rawAngleRadians))
+    });
+  };
+  const handleMoveEnd = () => {
+    setFlag(false);
+    onChange(Math.round(((max - min) / 90) * percent + min));
+  };
+
   return (
     <>
       <div className="relative flex items-center justify-center rounded-full p-6 shadow-[0_0_2rem_#f97316]">
@@ -56,28 +78,24 @@ function CircleSlider({ value = 50, min = 30, max = 60, onChange = (v) => {} }) 
               };
             }}
             onTouchMove={(e) => {
-              if (!flag) return;
               const dxTemp = e.touches[0].clientX - startPos.current.dx;
               const dyTemp = e.touches[0].clientY - startPos.current.dy;
-
-              // 计算拖动的弧度值
-              const rawAngleRadians = Math.atan2(dyTemp - center, dxTemp - center);
-              console.log('rawAngleRadians:', rawAngleRadians);
-              const percent = angleRadians2Percent(rawAngleRadians);
-              if (percent >= 0.9) {
-                return;
-              }
-              setPercent(() => Math.round(percent * 100));
-              // 根据角度和半径计算新的位置
-              setPosition({
-                dx: Math.round(radius + radius * Math.cos(rawAngleRadians)),
-                dy: Math.round(radius + radius * Math.sin(rawAngleRadians))
-              });
+              handleMove(dxTemp, dyTemp);
             }}
-            onTouchEnd={(e) => {
-              setFlag(false);
-              onChange(Math.round(((max - min) / 90) * percent + min));
+            onTouchEnd={handleMoveEnd}
+            onMouseDown={(e) => {
+              setFlag(true);
+              startPos.current = {
+                dx: e.clientX - position.dx,
+                dy: e.clientY - position.dy
+              };
             }}
+            onMouseMove={(e) => {
+              const dxTemp = e.clientX - startPos.current.dx;
+              const dyTemp = e.clientY - startPos.current.dy;
+              handleMove(dxTemp, dyTemp);
+            }}
+            onMouseUp={handleMoveEnd}
           >
             <div
               className="slider__ball border-solid absolute left-0 top-0 size-4 rounded-full border-2 bg-white"
@@ -93,17 +111,15 @@ function CircleSlider({ value = 50, min = 30, max = 60, onChange = (v) => {} }) 
           </div>
           <div className="overlay absolute bottom-4 left-4 right-4 top-4 z-10 rounded-full bg-gradient-to-tl from-[#f97b2c] to-[#f6b25e]"></div>
           <div
-            className="half__1 absolute left-0 top-0 z-[1] h-1/2 w-full bg-[#fa853c]"
+            className="half__1 absolute origin-[50%_100%] left-0 top-0 z-[1] h-1/2 w-full bg-[#fa853c]"
             style={{
-              transformOrigin: '50% 100%;',
               transform: `rotate(${(0.6 - 1) * 180}deg)`
             }}
           ></div>
           <div
-            className="half__2 absolute left-0 top-0 z-[2] h-1/2 w-full"
+            className="half__2 origin-[50%_100%] absolute left-0 top-0 z-[2] h-1/2 w-full"
             style={{
-              backgroundColor: percent / 100 > 0.5 ? '#fa853c' : 'inherit',
-              transformOrigin: '50% 100%;',
+              backgroundColor: percent / 100 > 0.5 ? '#fa853c' : 'white',
               transform: `rotate(${(percent / 100 > 0.5 ? (360 * percent) / 100 - 180 : (360 * percent) / 100) + (0.6 - 1) * 180}deg)`
             }}
           ></div>
